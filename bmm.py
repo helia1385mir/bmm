fe37283a4d52a2e5db9b23c8d5ff3e7593cb3ce6
helia1385mir <88113615+helia1385mir@users.noreply.github.com>
2021-08-10 12:23:49 +0430

    Add files via upload

---
+++ bmm.py
@@ -0,0 +1,17 @@
+a=int(input()) #12
+b=int(input())#30
+h=[]
+for i in range(1,a) :
+    if a %i==0 and b%i==0 :   #1=i
+        h=[i]
+        # print(b)
+bmm=max(h)
+print(bmm)
+if a>b :
+    int(input(print('dobare add bezarid add aval bayad add kochak bashad')))
+
+
+
+
+# bmm=max(i)
+
